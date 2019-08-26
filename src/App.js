70790b471d25bc83d790912e942e3b585eb8c4ba
Saju Paul <saju.christel@blu-pa.com>
2019-08-26 15:06:12 +0530

    adding person component, props and children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Lesen und Music Horen</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
   }
