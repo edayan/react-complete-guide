e72c5b396587a85c426078a6b76cca05b91d18f9
Saju Paul <saju.christel@blu-pa.com>
2019-11-06 21:27:49 +0530

    two different ways to pass arguments in to event handlers

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -12,10 +12,10 @@ class App extends Component {
     otherState: 'Some other value'
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = newName => {
     this.setState({
       persons: [
-        { name: 'maxmillion', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
@@ -27,7 +27,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
+        <button onClick={() => this.switchNameHandler('Mazxmillian!!')}>
+          Switch name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -35,7 +37,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}
+          click={this.switchNameHandler.bind(this, 'Max!')} //use bind sysntax as arrow function rerenders too many things
         >
           My Hobbies: Lesen und Music Horen
         </Person>
