e4184a3ec165f349612755adcc7a71c98550f0a1
Saju Paul <saju.christel@blu-pa.com>
2019-11-26 18:34:46 +0530

    Warpping two items when need to return without and extra div

--- src/components/Persons/Person/PersonComponent.js
+++ src/components/Persons/Person/PersonComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxilliary';
 
 export default class PersonComponent extends Component {
   constructor(props) {
@@ -9,24 +10,31 @@ export default class PersonComponent extends Component {
 
   render() {
     console.log('PersonComponent.js: render');
-    return [
+    return (
       /**
-      wraping multiple items into a [] and separating them with commas and them giving each items a key will work in the same way as restuning a single item.
+       * Instead of custom 'Aux' we can use React.Fragment.
        */
-      <div key={'i1'}>Person header</div>,
-      <div key={'i2'} className={classes.Person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          value={this.props.name}
-        />
-      </div>
-    ]; /**
-    warpping ends here
-     */
+      <Aux>
+        {/* [ */}
+        {/** wraping multiple items into a [] and separating them with commas
+        and them giving each items a key will work in the same way as returning
+        a single item. 'Aux' is not needed if we wrap with [] */}
+        <div key={'i1'}>Person header</div>
+        {/* , */}
+        <div key={'i2'} className={classes.Person}>
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old!
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </div>
+        {/* ];  */}
+        {/* * warpping ends here */}
+      </Aux>
+    );
   }
 }
